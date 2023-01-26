ce5170353304d334109fd1b5843dbe9136bd8fa1
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-26 20:34:29 +0700

    array

---
+++ array.js
@@ -0,0 +1,20 @@
+{
+    // const arr = ["rama", "fajar", "fadhillah"];
+
+    // arr.forEach((value, index) => {
+    //     console.info(`${index} : ${value}`);
+    // });
+}
+
+{
+    const queue = [];
+
+    queue.push("Rama");
+    queue.push("Fajar");
+    queue.push("Fadhillah");
+
+    console.info(queue.shift());
+    console.info(queue.shift());
+    console.info(queue.shift());
+    console.info(queue.shift());
+}
