1463c28b590caf544968a9276b38c2145f2893a2
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-26 20:34:22 +0700

    string

---
+++ string.js
@@ -0,0 +1,10 @@
+const name = "Rama Fajar Fadhillah";
+
+console.info(name.length);
+console.info(name.toLowerCase());
+console.info(name.toUpperCase());
+console.info(name.split(" "));
+
+const input = " input data ";
+console.info(input.trim());
+console.info(input);
