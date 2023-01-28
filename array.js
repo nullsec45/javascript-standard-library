c42eebce1e18e7e17b73f582279f888f8002de34
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-28 13:17:52 +0700

    array stack, array filter, array search

--- array.js
+++ array.js
@@ -7,14 +7,47 @@
 }
 
 {
+    // array queue
     const queue = [];
 
     queue.push("Rama");
     queue.push("Fajar");
     queue.push("Fadhillah");
 
-    console.info(queue.shift());
-    console.info(queue.shift());
-    console.info(queue.shift());
-    console.info(queue.shift());
+    // console.info(queue.shift());
+    // console.info(queue.shift());
+    // console.info(queue.shift());
+    // console.info(queue.shift());
+}
+{
+    // array stack
+    const stack = [];
+    stack.push("Rama");
+    stack.push("Fajar");
+    stack.push("Fadhillah");
+
+    // console.info(stack.pop())
+    // console.info(stack.pop());
+    // console.info(stack.pop());
+    // console.info(stack.pop());
+}
+{
+    // array search
+    const source = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+
+    console.info(source.find(value => value > 3));
+    // console.info(source.findIndex(value => value > 3));
+    // console.info(source.includes(7));
+    // console.info(source.indexOf(5));
+    // console.info(source.lastIndexOf(5));
+}
+{
+    // array filter
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const genap = numbers.filter(value => value % 2 === 0);
+    const ganjil = numbers.filter(value => value % 2 === 1);
+
+    console.info(numbers);
+    console.info(genap);
+    console.info(ganjil);
 }
