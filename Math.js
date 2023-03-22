796e85d77372e61f8c9e4bbbc48b8731f4465200
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-22 07:47:05 +0700

    Math

---
+++ Math.js
@@ -0,0 +1,11 @@
+// cari yang terbesar
+console.info(Math.max(10, 11, 23, 90, 11, 45, 23));
+// cari yang terendah
+console.info(Math.min(10, 11, 9, 13, 8));
+// pembulatan yang mendekati
+console.info(Math.round(10.5));
+console.info(Math.round(10.3));
+// pembulatan ke bawah
+console.info(Math.floor(10.8));
+// pembulatan ke atas
+console.info(Math.ceil(10.3));
